b5adb58dd459e781fe576a2d297caa2e7df32da2
kingpingx <prabal.pandey.prabal@gmail.com>
2022-01-13 17:34:08 +0530

    profile and complete profile fully created

--- avekshak/urls.py
+++ avekshak/urls.py
@@ -21,20 +21,28 @@ from mainapp import views
 from django.contrib.auth import views as auth_views
 from rest_framework import routers
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 # routers = routers.DefaultRouter()
 # routers.register(r'details', views.UserViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', GeneralPageView.as_view()),
-    path('home', HomeView.as_view()),
-    # path('user', include(routers.urls)),
-    # path('api_auth', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/', include('avekshak.urls_api')),
-    path('exercise', views.exercise),
+    path('register', RegisterView.as_view()),
     path('login', LoginView.as_view(), name="login"),
     path('forget_password/', views.forget_password, name='forget_password'),
     path('change_password/<token>/', views.change_password, name='change_password'),
-    path('register', RegisterView.as_view()),
+    path('home', HomeView.as_view()),
+    path('api/', include('avekshak.urls_api')),
+    path('exercise', views.exercise),
+    path('profile', ProfileView.as_view(), name="profile"),
+    path('profile/edit', EditProfileView.as_view(), name="edit_profile"),      
     path('logout', LogoutView.as_view()),
+    # path('user', include(routers.urls)),
+    # path('api_auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
